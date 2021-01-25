2c7f001ee1fc8dd3b7adc112a9ded51c8f046ba4
Pranshu Malhotra <pranshudee8@gmail.com>
2021-01-25 12:57:40 +0530

    video 2 : update RecyclerView

--- app/src/main/java/com/example/hpgame/MainActivity.kt
+++ app/src/main/java/com/example/hpgame/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.hpgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +21,9 @@ class MainActivity : AppCompatActivity() {
         numMoves = findViewById(R.id.moves_textview);
         numPairs = findViewById(R.id.pairs_textview);
 
-        
+        board.adapter = MemoryBoardAdapter(this, 8);
+        board.setHasFixedSize(true);
+        board.layoutManager = GridLayoutManager(this, 2);
 
 
     }
